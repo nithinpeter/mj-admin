73ed02e34d40be7793847a2e878f0399fcb3fe58
Nithin Peter <nithinpeter7@gmail.com>
2016-04-28 22:49:03 +1000

    Bug fix: silly server code error.

--- server/index.js
+++ server/index.js
@@ -9,6 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile( path.resolve(__dirname, '../index.html'));
 });
 
-app.listen(3010, function () {
+app.listen(3010 || process.env.PORT, function () {
   console.log('Example app listening on port 3010!');
 });
